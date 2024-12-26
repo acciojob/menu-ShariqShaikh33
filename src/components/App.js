567dc5a74845b9651a51a123d7232238a5cc5adc
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-26 20:39:19 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -109,10 +109,11 @@ const App =()=>{
         <div>
             <h1 className="headingTitle">Our Menu</h1>
             <div className="headingTabs">
-                <button className="main" onClick={showAll} >All</button>
-                <button className="filter-btn-1" onClick={showBreakfast}>Breakfast</button>
-                <button className="filter-btn-2" onClick={showLunch}>Lunch</button>
-                <button className="filter-btn-3" onClick={showShakes}>Shakes</button>
+                    <button id="main" className="main" onClick={showAll} >All</button>
+                    <button id="filter-btn-1" className="filter-btn-1" onClick={showBreakfast}>Breakfast</button>
+                    <button id="filter-btn-2" className="filter-btn-2" onClick={showLunch}>Lunch</button>
+                    <button id="filter-btn-3" className="filter-btn-3" onClick={showShakes}>Shakes</button>
+                
             </div>
 
             <div className="Fooddiv">
